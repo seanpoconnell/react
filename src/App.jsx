f57e1a570e4f54efebe8d6cd96274d1f5e8d4b58
sean oconnell <seanoconnell96@gmail.com>
2020-09-24 18:09:18 +0100

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Hello from './Hello'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Hello />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react' ;
+
+const Hello = () => {
+  return (
+    <h1>Goddbye World</h1>
+  );
+}
+
+export default Hello;
