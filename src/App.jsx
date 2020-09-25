1c9504e1abc83cebdc8cb46a31b85de6e8a12b59
sean oconnell <seanoconnell96@gmail.com>
2020-09-25 14:54:14 +0100

    added input box for user to enter name

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Hello from './Hello'
+import Name from './Name'
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Name />
         <Hello name="Sean" age={23} />
         <Hello name="Ashley" age={17} />
         <Hello name="Chris" age={27} />
--- src/Name.jsx
+++ src/Name.jsx
@@ -1,1 +1,14 @@
+import React, { Component } from 'react' ;
 
+class Name extends Component {
+  render() {
+    return(
+      <div>
+        <input type="text" />
+        <p>NAME</p>
+      </div>
+    );
+  }
+}
+
+export default Name;
