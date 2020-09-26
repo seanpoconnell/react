4602008a94c2414856d1bf7838c2ee76d83db6ff
sean oconnell <seanoconnell96@gmail.com>
2020-09-26 15:41:53 +0100

    counter numbers go up and down but not lower than 0

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import './Counter.scss';
 
 class Counter extends Component {
+  state = {
+    count: 0
+  }
+
+  subtract = () => {
+    if (this.state.count === 0) {
+      return;
+    }
+    this.setState({ count: this.state.count - 1});
+  }
+
+  add = () => {
+    this.setState({ count: this.state.count + 1});
+  }
+
   render() {
     return (
       <div className="counter">
-        <button>-</button>
-        <span className="count">NUMBER</span>
-        <button>+</button>
+        <button onClick={this.subtract}>-</button>
+        <span className="count">{this.state.count}</span>
+        <button onClick={this.add}>+</button>
       </div>
     )
   }
