a088b0535f7ea60ec29a76a9d45a1b880750543f
sean oconnell <seanoconnell96@gmail.com>
2020-09-26 15:13:10 +0100

    display user input name

--- src/Name.jsx
+++ src/Name.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react' ;
 
 class Name extends Component {
+  state = {
+    name: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({name: event.target.value})
+  }
+
+  resetName = () => {
+    this.setState({ name: "" })
+  }
+
+  // everytime your state or props change the render will run again
   render() {
     return(
       <div>
-        <input type="text" />
-        <p>NAME</p>
+        <input type="text" onChange={this.handleChange} value={this.state.name}/>
+        <button onClick={this.resetName}>Reset!</button>
+        <p>{this.state.name}</p>
       </div>
     );
   }
